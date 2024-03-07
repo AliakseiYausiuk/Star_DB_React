8e8f88caf0bf99844848d93ef5e24eecc9dd2468
AliakseiYausiuk <AliakseiYausiuk@gmail.com>
2024-03-07 20:39:17 +0300

    fix: change class for definition expression in component ItemList

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -4,48 +4,33 @@ import Spinner from "../spinner/spinner";
 
 import "./item-list.css";
 
-export default class ItemList extends Component {
-  state = {
-    itemList: null,
-  };
+const ItemList = (props) => {
+  const { data, onItemSelected, children: renderLabel } = props;
 
-  componentDidMount() {
-    const { getData } = this.props;
-
-    getData().then((itemList) => {
-      this.setState({ itemList });
-    });
-  }
-
-  renderItems(arr) {
-    return arr.map((item) => {
-      const { id } = item;
-      const label = this.props.children(item);
-      return (
-        <li
-          key={id}
-          className="list-group-item"
-          onClick={() => this.props.onItemSelected(id)}
-        >
-          {label}
-        </li>
-      );
-    });
-  }
-
-  render() {
-    const { itemList } = this.state;
-
-    if (!itemList) {
-      return <Spinner />;
-    }
-
-    const itens = this.renderItems(itemList);
+  const items = data.map((item) => {
+    const { id } = item;
+    const label = renderLabel(item);
 
     return (
-      <div className="item-list-div">
-        <ul className="item-list list-group">{itens}</ul>
-      </div>
+      <li
+        className="list-group-item"
+        key={id}
+        onClick={() => onItemSelected(id)}
+      >
+        {label}
+      </li>
     );
+  });
+
+  if (!items) {
+    return <Spinner />;
   }
-}
+
+  return (
+    <div className="item-list-div">
+      <ul className="item-list list-group">{items}</ul>
+    </div>
+  );
+};
+
+export default ItemList;
