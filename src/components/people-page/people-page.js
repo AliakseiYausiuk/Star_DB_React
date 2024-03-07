3d8793764e49ef2442fde5d19c0f868259276f42
AliakseiYausiuk <AliakseiYausiuk@gmail.com>
2024-03-07 20:40:48 +0300

    fix: link and add api in component PeoplePage

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 
 import "./people-page.css";
 
+import SwapiService from "../../services/swapi-service";
 import ItemList from "../item-list/item-list";
-import PersonDetails from "../person-details/person-details";
+import PersonDetails from "../item-details/item-details";
 import ErrorIndicator from "../error-indicator/error-indicator";
 import Row from "../row/row";
 import ErrorBoundry from "../error-boundry/error-boundry";
 
 export default class PeoplePage extends Component {
+  swapiService = new SwapiService();
+
   state = {
     selectedPerson: 11,
   };
