33d83c29be04a9b3b85c91a74fbd64880bd2f688
AliakseiYausiuk <AliakseiYausiuk@gmail.com>
2024-03-07 11:10:54 +0300

    feat: add error output for the user

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 
+import "./app.css";
+
 import Header from "../header";
 import RandomPlanet from "../random-planet";
 import ItemList from "../item-list";
 import PersonDetails from "../person-details";
-
-import "./app.css";
+import ErrorButton from "../error-button/error-button";
+import ErrorIndicator from "../error-indicator/error-indicator";
 
 export default class App extends Component {
   state = {
     selectedPerson: 11,
+    hasError: false,
   };
 
   onPersonSelected = (id) => {
@@ -18,11 +21,23 @@ export default class App extends Component {
     });
   };
 
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
+
   render() {
+    if (this.state.hasError) {
+      return <ErrorIndicator />;
+    }
+
     return (
       <div>
         <Header />
         <RandomPlanet />
+
+        <div className="button-row">
+          <ErrorButton />
+        </div>
 
         <div className="row col-md-12">
           <div className="col-md-4">
