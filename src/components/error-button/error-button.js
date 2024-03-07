d03447c240fca97722369c3e135ca4c0cd47691e
AliakseiYausiuk <AliakseiYausiuk@gmail.com>
2024-03-07 11:11:47 +0300

    fix: error-button

--- src/components/error-button/error-button.js
+++ src/components/error-button/error-button.js
@@ -11,9 +11,13 @@ export default class ErrorButton extends Component {
       this.foo.bar = 0;
     }
 
-    <button
-      className="error-button btn btn-danger btn-lg"
-      onClick={() => this.setState({ renderError: true })}
-    ></button>;
+    return (
+      <button
+        className="error-button btn btn-danger btn-lg"
+        onClick={() => this.setState({ renderError: true })}
+      >
+        Throw Error
+      </button>
+    );
   }
 }
