0760a7455c852473d9db08450cbaec0d4d6afa35
AliakseiYausiuk <AliakseiYausiuk@gmail.com>
2024-03-11 15:01:21 +0300

    fix: deleted not use code in ItemList

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,6 +1,4 @@
 import React from "react";
-import SwapiService from "../../services/swapi-service";
-import withData from "../hoc-helpers/with-data";
 
 import Spinner from "../spinner/spinner";
 
@@ -35,6 +33,4 @@ const ItemList = (props) => {
   );
 };
 
-const { getAllPeople } = new SwapiService();
-
-export default withData(ItemList, getAllPeople);
+export default ItemList;
