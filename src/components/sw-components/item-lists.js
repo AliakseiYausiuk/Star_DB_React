2338c3fe571ea48f8b0f84e81db82260484ad7f1
AliakseiYausiuk <AliakseiYausiuk@gmail.com>
2024-03-11 15:01:50 +0300

    feat: add new logic and components

---
+++ src/components/sw-components/item-lists.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import withData from "../hoc-helpers/with-data";
+import SwapiService from "../../services/swapi-service";
+import ItemList from "../item-list/item-list";
+
+const swapiService = new SwapiService();
+
+const { getAllPeople, getAllStarships, getAllPlanets } = swapiService;
+
+const withChildFunction = (Wrapped, fn) => {
+  return (props) => {
+    return <Wrapped {...props}>{fn}</Wrapped>;
+  };
+};
+
+const ListWithChildren = withChildFunction(ItemList, ({ name }) => (
+  <span>{name}</span>
+));
+
+const PersonList = withData(ListWithChildren, getAllPeople);
+const PlanetList = withData(ListWithChildren, getAllPlanets);
+const StarshipList = withData(ListWithChildren, getAllStarships);
+
+export { PersonList, PlanetList, StarshipList };
