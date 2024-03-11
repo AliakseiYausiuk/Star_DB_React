2e985b9df48c04c6998d82d911d6f1601259c848
AliakseiYausiuk <AliakseiYausiuk@gmail.com>
2024-03-11 13:47:26 +0300

    feat: add new logic in component App

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,8 +7,15 @@ import RandomPlanet from "../random-planet";
 import ErrorButton from "../error-button/error-button";
 import ErrorIndicator from "../error-indicator/error-indicator";
 import PeoplePage from "../people-page/people-page";
+import SwapiService from "../../services/swapi-service";
+import ErrorBoundry from "../error-boundry/error-boundry";
+import Row from "../row/row";
+import ItemDetails from "../item-details/item-details";
+import { Record } from "../item-details/item-details";
 
 export default class App extends Component {
+  swapiService = new SwapiService();
+
   state = {
     hasError: false,
   };
@@ -28,16 +35,44 @@ export default class App extends Component {
       return <ErrorIndicator />;
     }
 
+    const {
+      getPerson,
+      getAllPeople,
+      getStarships,
+      getPersonImage,
+      getStarshipImage,
+    } = this.swapiService;
+
+    const personDetails = (
+      <ItemDetails itemId={11} getData={getPerson} getImageUrl={getPersonImage}>
+        <Record field="gender" label="Gender" />
+        <Record field="eyeColor" label="Eye Color" />
+      </ItemDetails>
+    );
+
+    const starshipDetails = (
+      <ItemDetails
+        itemId={1}
+        getData={getStarships}
+        getImageUrl={getStarshipImage}
+      >
+        <Record field="model" label="Model" />
+        <Record field="length" label="Length" />
+        <Record field="costInCredits" label="Cost" />
+      </ItemDetails>
+    );
+
     return (
-      <div>
+      <ErrorBoundry>
         <Header />
         <RandomPlanet />
 
         <div className="button-row">
           <ErrorButton />
         </div>
-        <PeoplePage />
-      </div>
+        <PeoplePage getData={getAllPeople} />
+        <Row left={personDetails} right={starshipDetails} />
+      </ErrorBoundry>
     );
   }
 }
